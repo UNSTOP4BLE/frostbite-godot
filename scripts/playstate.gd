512900f2135ab86fb29035ed9bd81285c1b9b911
UNSTOP4BLE <88965856+UNSTOP4BLE@users.noreply.github.com>
2022-11-26 12:03:38 +0100

    o

--- scripts/playstate.gd
+++ scripts/playstate.gd
@@ -22,9 +22,12 @@ func resumeVocal():
 func Gold_SetAnim(var anim):
 	$Gold.get_node("AnimationPlayer").set_current_animation(anim)	
 	$Gold.get_node("AnimationPlayer").play()
-	
-		
 
+# note functions
+func Note_SetAnim(var i, var anim):
+	$PDummyNotes.get_node(str(i)).get_node("AnimationPlayer").set_current_animation(anim)	
+	$PDummyNotes.get_node(str(i)).get_node("AnimationPlayer").play()
+		
 # game functions
 func playStateInit():
 	getVocal().stream = Vocals
@@ -33,6 +36,16 @@ func playStateInit():
 	getInst().play()
 	
 	$Gold.get_node("AnimationPlayer").set_speed_scale(30)
+	
+	for i in 4:
+		$PDummyNotes.get_node(str(i)).get_node("AnimationPlayer").set_speed_scale(30)
+		
+	
+	Note_SetAnim(0, "left-dummy");
+	Note_SetAnim(1, "down-dummy");
+	Note_SetAnim(2, "up-dummy");
+	Note_SetAnim(3, "right-dummy");
+	
 	Gold_SetAnim("gold-idle")
 
 var bump = false;
@@ -40,6 +53,8 @@ var checkInput = [0, 0, 0, 0];
 
 func playState(delta):
 	bump = scene().songStep % 8 == 7
+	
+	# Draw notes
 	
 	checkInput[0] = Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT)
 	checkInput[1] = Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN)
